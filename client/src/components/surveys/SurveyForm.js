8bb2650a8bee122005050cd38b86379500dc9952
Agbai Iroha <agbai@myxrevents.com>
2022-07-24 23:45:03 -0400

    started on form creation front end

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,71 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
+import { Link } from 'react-router-dom';
+import validateEmails from '../../utils/validateEmails';
+
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipient List', name: 'emails' },
+];
+
+class SurveyForm extends Component {
+  renderFields() {
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form
+          onSubmit={this.props.handleSubmit((values) => {
+            console.log('values: ', values);
+          })}
+        >
+          {this.renderFields()}
+          <Link to="/surveys" className="red btn-flat left white-text">
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const validate = (values) => {
+  const errors = {};
+
+  // throw error if emails are not valid
+  errors.emails = validateEmails(values.emails || '');
+
+  // check if all fields are poopulated
+  _.each(FIELDS, ({ name }) => {
+    if (!values[name]) {
+      errors[name] = 'You must provide a value.';
+    }
+  });
+
+  return errors;
+};
+
+export default reduxForm({
+  validate,
+  form: 'surveyForm',
+})(SurveyForm);
